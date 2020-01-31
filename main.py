12713362614eceeb3cb3db72c0ea181bc32047ed
Priyanshu Dwivedi <ankurdwivedi75@gmail.com>
2020-02-01 01:52:13 +0530

    updating run.py and renaming to main

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import requests, json
 import pprint
 data = {
     "image_url":input(),
-    "resized_images":True # Or true
 }
 headers = {'Content-type': 'application/json'}
 data = requests.post("http://ec2-18-221-131-219.us-east-2.compute.amazonaws.com:5000/search", headers=headers, data=json.dumps(data)).json()
